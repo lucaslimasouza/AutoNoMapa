731b2ed348ff353e4a13c573b992a18b13809029
lucaslimasouza <lucaslima4p@gmail.com>
2013-02-04 10:27:14 -0200

    Tests(fields and associations) of Category Model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Category do
   context "#fields" do
     it { should respond_to(:name) }
+    it { should respond_to(:father_id) }
+  end
 
+  context "#association" do
+    it { should have_many(:subcategories) }
+    it { should belong_to(:father) }
+    it { should have_and_belong_to_many(:profiles) }
   end
 end
