8c927a2a0552858cc299d3bdd73ef47509a59d4a
lucaslimasouza <lucaslima4p@gmail.com>
2013-02-04 10:47:57 -0200

    Tests Indication#fields and association

--- spec/models/indication_spec.rb
+++ spec/models/indication_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Indication do
-  
+
+  context "#association" do
+    it { should belong_to(:searcher) }
+    it { should belong_to(:freelance) }
+  end
+
+  context "#validation" do
+    it { should validate_presence_of(:searcher_id) }
+    it { should validate_presence_of(:freelance_id) }
+  end
+
 end
