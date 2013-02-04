02662127204e79f44ef5d6c528d752fef4628d20
lucaslimasouza <lucaslima4p@gmail.com>
2013-02-04 10:59:16 -0200

    Tests Searcer#association

--- spec/models/searcher_spec.rb
+++ spec/models/searcher_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Searcher do
   let(:freelance) {mock_model(Freelance)}
+
+  context "#association" do
+    it { should have_many(:indications) }
+  end
+
   context "do some indication" do
   	it "should indicate a @freelance" do
   	  searcher = Searcher.new(email: "searcher@mail.com", password: "1a2b3c",password_confirmation: "1a2b3c")
@@ -9,7 +14,7 @@ describe Searcher do
   	  searcher.indicate(freelance);
   	  searcher.indications.should_not be_nil
   	end
-    
+
   end
 
 end
