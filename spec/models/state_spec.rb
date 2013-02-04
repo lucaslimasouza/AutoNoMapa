464ed03561318d07823273d48eecad3c70882b9a
lucaslimasouza <lucaslima4p@gmail.com>
2013-02-04 11:01:59 -0200

    Tests State#fields and association

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe State do
+  context "#fields" do
+    it { should respond_to(:acronym)}
+    it { should respond_to(:name) }
+    it { should respond_to(:city) }
+  end
+
+  context "#association" do
+    it { should have_many(:city) }
+  end
 
 end
