7fb7bab02d57974dff66f9c32e280f972ebe8db4
lucaslimasouza <lucaslima4p@gmail.com>
2013-02-04 10:30:07 -0200

    Tests City#fields

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe City do
+  context "#fields" do
+    it { should respond_to(:capital) }
+    it { should respond_to(:name) }
+    it { should respond_to(:state) }
+  end
 
 end
