1a9242555ccf6aee982de82ad0edea9bc11367c4
lucaslimasouza <lucaslima4p@gmail.com>
2013-03-15 21:58:36 -0300

    try fixing Profile#search again (postgle) capitalize

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,7 +19,7 @@ class Profile < ActiveRecord::Base
      self.find :all, :joins => [:categories, :city],
        :conditions =>
        ['lower(cities.name) like :city and lower(categories.name) like :category' ,
-        {:city => "#{profile[:city].capitalize}" , :category => "%#{profile[:category]}%"}]
+        {:city => "#{profile[:city].capitalize}" , :category => "%#{profile[:category].capitalize}%"}]
   end
 
   private
